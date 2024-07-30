4f266c6d2d495a44c0cd7863b1d44fef6da82628
JrBrandonOrtiz <165676059+JrBrandonOrtiz@users.noreply.github.com>
2024-07-30 13:19:00 -0500

    Delete CSV-reader-main directory

--- CSV-reader-main/src/models/models.ts
+++
@@ -1,7 +0,0 @@
-export interface DataRow { // componne cada fila, dándole formato de clave/valor
-    [key:string]:string
-};
-
-export type DataTable = DataRow[]; // compone la tabla, dándole formato de filas ( DataRow )
-
-export type ColumnName = string[]; // compondrá las columnas, títulos
